840ca1c8a03f6dc01ba117bd2b3c84e8262c0962
Yago <150179517+Ybrittoom@users.noreply.github.com>
2024-07-05 13:07:44 -0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,3 +1,12 @@
+function darkMode() {
+    document.body.classList.toggle('dark-mode');
+    document.querySelector('header').classList.toggle('dark-mode');
+    document.querySelector('main').classList.toggle('dark-mode');
+    document.querySelector('footer').classList.toggle('dark-mode');
+}
+
+
+
 const btn = document.getElementById("botao");
 
 function apertarBotao() {
